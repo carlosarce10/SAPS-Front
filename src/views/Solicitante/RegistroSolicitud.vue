f01b7508d0baba57ed21b9f80e447c00cbdb59ea
Alan Garcia <aalangarcia.1998@gmail.com>
2021-11-30 21:30:13 -0600

    Registro solicitud

--- src/views/Solicitante/RegistroSolicitud.vue
+++ src/views/Solicitante/RegistroSolicitud.vue
@@ -6,7 +6,7 @@
     <div>
       <div class="container pt-4">
         <div class="row perfil rounded">
-          <div class="col-12 float-start">
+          <div class="col-8 float-start">
             <h4 class="float-start p-1">Registro de solicitud</h4>
           </div>
         </div>
@@ -19,7 +19,7 @@
       class="
         row
         inicioSesion
-        col-7
+        col-8
         position-absolute
         top-50
         start-50
@@ -30,10 +30,17 @@
         rounded
       "
     >
-    <form>
-      <div class="mb-3 col-4">
+      <div class="mb-3 col-12">
+        <label for="dateSolicitud" class="float-start">Elige una fecha</label>
+        <b-form-datepicker id="dateSolicitud" v-model="dateSolicitud" class="mb-2 form-select"></b-form-datepicker>
+      </div>
+      <div class="mb-3 col-12">
+        <label for="timeSolicitud" class="float-start">Elige una hora</label>
+        <b-form-timepicker id="timeSolicitud" v-model="timeSolicitud" locale="es"></b-form-timepicker>
+      </div>
+      <div class="mb-3 col-12">
         <label class="float-start">Motivo</label>
-        <b-form-select required v-model="motivo" class="form-select">
+        <b-form-select required v-model="motivos" multiple :select-size="listaMotivos.length" class="form-select">
           <option
             v-for="motivo in listaMotivos"
             v-bind:key="motivo.motivo"
@@ -41,12 +48,11 @@
           >
             {{ motivo.motivo }}
           </option>
-          <b-form-select-option value="" disabled>Elige una opción</b-form-select-option>
         </b-form-select>
       </div>
-      <div class="mb-3 col-4">
+      <div class="mb-3 col-12">
         <label class="float-start">Sintomas</label>
-        <b-form-select required v-model="sintoma" class="form-select">
+        <b-form-select required v-model="sintomas" multiple :select-size="listaSintomas.length" class="form-select">
           <option
             v-for="sintoma in listaSintomas"
             v-bind:key="sintoma.sintoma"
@@ -54,19 +60,39 @@
           >
             {{ sintoma.sintoma }}
           </option>
+        </b-form-select>
+      </div>
+      <div class="mb-3 col-12">
+        <label class="float-start">Consultores</label>
+        <b-form-select required v-model="consultor" class="form-select">
+          <option
+            v-for="consultor in consultores"
+            v-bind:key="consultor.consultor"
+            v-bind:value="consultor.idConsultor"
+          >
+            {{ consultor.usuario.nombre + ' ' + consultor.usuario.apellidoPaterno + ' ' + consultor.usuario.apellidoMaterno }}
+          </option>
           <b-form-select-option value="" disabled>Elige una opción</b-form-select-option>
         </b-form-select>
       </div>
-      <div class="mt-4 col-4">
-        <button
-          type="submit"
+      <div class="mb-3 col-12">
+        <b-button
+          variant="primary"
+          type="button"
           @click="registroSolicitud()"
-          class="btn btn-primary m-1 float-end"
+          class="m-1 float-end"
         >
           Registrarme
-        </button>
+        </b-button>
+        <b-button
+          variant="danger"
+          type="button"
+          href="/solicitante/inicio"
+          class="m-1 float-end"
+        >
+          Cancelar
+        </b-button>
       </div>
-      </form>
     </div>
   </div>
 </template>
@@ -87,18 +113,23 @@ export default {
     return {
       listaMotivos: [],
       listaSintomas: [],
+      consultores: [],
       form: {
       },
-      motivo: '',
-      sintoma: '',
-      correo: '',
+      dateSolicitud: '',
+      timeSolicitud: '',
+      time: '',
+      motivos: [],
+      sintomas: [],
+      consultor: '',
+      correo: localStorage.getItem("username"),
       idSolicitante:'',
       idUsuario: '',
     };
   },
   beforeMount() {
-    this.correo = localStorage.getItem("username");
     this.getUser();
+    this.getConsultores();
     this.getSintomas();
     this.getMotivos();
   },
@@ -143,25 +174,69 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
+    getConsultores() {
+      api
+        .doGet('saps/consultor/getAll')
+        .then((response) => (this.consultores = response.data))
+        .catch((error) => {
+          let errorResponse = error.response.data;
+          if (errorResponse.errorExists) {
+            this.$swal({
+              title: 'Oops! Ha ocurrido un error en el servidor.',
+              icon: 'error',
+            });
+          } else {
+            this.$swal({
+              title: 'Oops! Ha ocurrido un error en el servidor.',
+              icon: 'error',
+            });
+          }
+        })
+        .finally(() => (this.loading = false));
+    },
     registroSolicitud() {
-    this.form = {
+      let listMotivos = [];
+      let listSintomas = [];
+
+      this.motivos.forEach(m => {
+        listMotivos.push({'idMotivo': m})
+      });
+
+      this.sintomas.forEach(s => {
+        listSintomas.push({'idSintoma': s})
+      });
+
+      this.form = {
         fecha: fechaActual,
+        estado: 'Pendiente',
         solicitante: {
             idSolicitante: this.idSolicitante,
         },
-        estado: 'Pendiente',
-        motivo: this.motivo,
-        sintoma: this.sintoma,
-      },
+        motivos: listMotivos, 
+        sintomas: listSintomas, 
+        consultor: {
+          idConsultor: this.consultor
+        }
+      }
       api
         .doPost('saps/solicitud/save', this.form)
         .then(() => {
-            console.log(this.form.fecha);
           this.$swal({
-            title: 'Tu solicitud se ha registrado de manera exitosa!',
+            title: 'La solicitud se ha registrado correctamente',
             icon: 'success',
           });
-          this.onReset();
+          this.$swal({
+            title: 'Solicitud registrada',
+            text: "La solicitud ha sido generada y enviada correctamente",
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.$router.push("/solicitante/inicio");
+            }
+          })
+          
         })
         .catch((error) => {
           let errorResponse = error;
@@ -201,13 +276,13 @@ export default {
             (this.idSolicitante = response.data[0].idSolicitante)
           )
         )
-        .then(() => console.log(this.show));
+        .then((e) => console.log(e));
     },
     onReset() {
-      this.form.fecha = '';
-      this.form.motivo = '';
-      this.form.sintoma = '';
-    },
+      this.form = {}
+      this.getSintomas();
+      this.getMotivos();
+    }
   },
 };
 </script>
